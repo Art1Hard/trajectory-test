ba1b4a83d0bf1e499cb01c5d1b9bde2e8f1d0e35
hardov <1artem1nazarov1@gmail.com>
2025-07-29 16:43:31 +0300

    change url map to dark

--- src/components/car/CarLocatorMap.tsx
+++ src/components/car/CarLocatorMap.tsx
@@ -10,8 +10,8 @@ const CarLocatorMap = ({ cars }: { cars: ICar[] }) => {
 			zoom={6}
 			scrollWheelZoom={false}>
 			<TileLayer
-				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a>, &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+				url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png"
 			/>
 			{cars.map((car) => (
 				<Marker key={car.id} position={[car.latitude, car.longitude]}>
